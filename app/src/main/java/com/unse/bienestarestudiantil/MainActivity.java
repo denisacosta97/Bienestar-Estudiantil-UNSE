ea1d351a8b50f3456ed1ebe208093a0a2a4609c3
Denis Acosta <denisacosta97@hotmail.com>
2019-09-22 18:21:30 -0300

    Prueba de dos actualizaciones conjuntas

--- app/src/main/java/com/unse/bienestarestudiantil/MainActivity.java
+++ app/src/main/java/com/unse/bienestarestudiantil/MainActivity.java
@@ -1,8 +1,8 @@
 package com.unse.bienestarestudiantil;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //holi chau
+
+        int b;
     }
 }
