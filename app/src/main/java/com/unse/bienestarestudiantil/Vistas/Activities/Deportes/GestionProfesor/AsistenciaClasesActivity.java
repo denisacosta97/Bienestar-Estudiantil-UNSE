0b77e1ac630a195dcd4ebb00f69ce8cc48ef1527
Denis Acosta <denisacosta97@hotmail.com>
2019-11-21 15:25:06 -0300

    No se que cambioos, capaz expresion regular jaja

--- app/src/main/java/com/unse/bienestarestudiantil/Vistas/Activities/Deportes/GestionProfesor/AsistenciaClasesActivity.java
+++ app/src/main/java/com/unse/bienestarestudiantil/Vistas/Activities/Deportes/GestionProfesor/AsistenciaClasesActivity.java
@@ -1,10 +1,8 @@
 package com.unse.bienestarestudiantil.Vistas.Activities.Deportes.GestionProfesor;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,25 +11,23 @@ import android.widget.TextView;
 
 import com.unse.bienestarestudiantil.Herramientas.RecyclerListener.ItemClickSupport;
 import com.unse.bienestarestudiantil.Herramientas.Utils;
-import com.unse.bienestarestudiantil.Modelos.Opciones;
 import com.unse.bienestarestudiantil.R;
-import com.unse.bienestarestudiantil.Vistas.Activities.AsistenciaActivity;
-import com.unse.bienestarestudiantil.Vistas.Adaptadores.OpcionesAdapter;
+import com.unse.bienestarestudiantil.Vistas.Adaptadores.AsistenciaClasesAdapter;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class GestionProfesorActivity extends AppCompatActivity implements View.OnClickListener {
+public class AsistenciaClasesActivity extends AppCompatActivity implements View.OnClickListener {
 
     RecyclerView mRecyclerView;
     RecyclerView.LayoutManager mLayoutManager;
-    OpcionesAdapter mAdapter;
-    ArrayList<Opciones> mOpciones;
+    AsistenciaClasesAdapter mAdapter;
     ImageView imgIcono;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gestion_profesor);
+        setContentView(R.layout.activity_asistencia_clases);
 
         Utils.setFont(getApplicationContext(), (ViewGroup) findViewById(android.R.id.content), Utils.MONSERRAT);
 
@@ -57,31 +53,40 @@ public class GestionProfesorActivity extends AppCompatActivity implements View.O
         itemClickSupport.setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
             @Override
             public void onItemClick(RecyclerView parent, View view, int position, long id) {
-                switch ((int) id){
-                    case 1:
-                        startActivity(new Intent(getApplicationContext(), ListadoAlumnosActivity.class));
-                        break;
-                    case 2:
-                        startActivity(new Intent(getApplicationContext(), AsistenciaClasesActivity.class));
-                        break;
 
-                }
-                Utils.showToast(getApplicationContext(), "Item: "+mOpciones.get(position).getTitulo());
             }
         });
         imgIcono.setOnClickListener(this);
     }
 
     private void loadData() {
-        mOpciones = new ArrayList<>();
-        mOpciones.add(new Opciones(1,"Listado de Alumnos",R.drawable.ic_usuarios, R.color.colorFCEyT ));
-        mOpciones.add(new Opciones(2, "Asistencias",R.drawable.ic_listado, R.color.colorFCEyT));
-        mOpciones.add(new Opciones(3, "Gestión de Torneos", R.drawable.ic_torneo, R.color.colorFCEyT));
 
-        mLayoutManager = new GridLayoutManager(getApplicationContext(), 2);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mAdapter = new OpcionesAdapter(mOpciones, getApplicationContext());
-        mRecyclerView.setAdapter(mAdapter);
+
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(),4));
+
+        mAdapter = new AsistenciaClasesAdapter(getApplicationContext(), mRecyclerView);
+
+        List<AsistenciaClasesAdapter.Meses> sections =
+                new ArrayList<AsistenciaClasesAdapter.Meses>();
+
+        //Sections
+        sections.add(new AsistenciaClasesAdapter.Meses(0,"Marzo"));
+        sections.add(new AsistenciaClasesAdapter.Meses(5,"Abril"));
+        sections.add(new AsistenciaClasesAdapter.Meses(12,"Mayo"));
+        sections.add(new AsistenciaClasesAdapter.Meses(14,"Junio"));
+        sections.add(new AsistenciaClasesAdapter.Meses(20,"Julio"));
+
+        //Add your adapter to the sectionAdapter
+        AsistenciaClasesAdapter.Meses[] dummy = new AsistenciaClasesAdapter.Meses[sections.size()];
+        AsistenciaClasesAdapter mSectionedAdapter = new
+                AsistenciaClasesAdapter(getApplicationContext(),
+                mRecyclerView,mAdapter);
+        mSectionedAdapter.setSections(sections.toArray(dummy));
+
+        //Apply this adapter to the RecyclerView
+        mRecyclerView.setAdapter(mSectionedAdapter);
+
     }
 
     private void loadViews() {
@@ -98,3 +103,4 @@ public class GestionProfesorActivity extends AppCompatActivity implements View.O
         }
     }
 }
+
---
+++ app/src/main/java/com/unse/bienestarestudiantil/Vistas/Adaptadores/AsistenciaClasesAdapter.java
@@ -0,0 +1,172 @@
+package com.unse.bienestarestudiantil.Vistas.Adaptadores;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.unse.bienestarestudiantil.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class AsistenciaClasesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private final Context mContext;
+    private static final int TYPE_MES = 0;
+    private static final int TYPE_CLASE = 0;
+
+    private boolean mValid = true;
+    private SparseArray<Meses> mSections = new SparseArray<Meses>();
+    private ArrayList<Integer> clases;
+    private RecyclerView mRecyclerView;
+
+
+    public AsistenciaClasesAdapter(Context context,
+                                   RecyclerView recyclerView) {
+
+        mContext = context;
+        mRecyclerView = recyclerView;
+        clases = new ArrayList<>();
+        final GridLayoutManager layoutManager = (GridLayoutManager)(mRecyclerView.getLayoutManager());
+        assert layoutManager != null;
+        layoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                return (isSectionHeaderPosition(position))? layoutManager.getSpanCount() : 1 ;
+            }
+        });
+    }
+
+    class ClasesViewHolder extends RecyclerView.ViewHolder {
+        private TextView txtClase;
+
+        public ClasesViewHolder(View itemView) {
+            super(itemView);
+            txtClase = itemView.findViewById(R.id.txtTitulo);
+        }
+    }
+
+    class MesesViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView txtTitulo;
+
+         MesesViewHolder(View view) {
+            super(view);
+            txtTitulo = view.findViewById(R.id.txtTitulo);
+        }
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int typeView) {
+
+        View view = null;
+
+        if (typeView == TYPE_MES) {
+
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_asistencia_mes, parent, false);
+            return new MesesViewHolder(view);
+
+        }else{
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_asistencia_clase, parent, false);
+            return new ClasesViewHolder(view);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder sectionViewHolder, int position) {
+
+        if (isSectionHeaderPosition(position)) {
+            MesesViewHolder holder = (MesesViewHolder) sectionViewHolder;
+            holder.txtTitulo.setText(mSections.get(position).getTitle());
+        }else{
+            ClasesViewHolder holder = (ClasesViewHolder) sectionViewHolder;
+            holder.txtClase.setText("Clase "+position);
+        }
+
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return isSectionHeaderPosition(position)
+                ? TYPE_MES
+                :  TYPE_CLASE;
+    }
+
+
+    public static class Meses {
+        private int firstPosition;
+        private int sectionedPosition;
+        private String txtTitulo;
+
+        public Meses(int firstPosition, String title) {
+            this.firstPosition = firstPosition;
+            this.txtTitulo = title;
+        }
+
+        public String  getTitle() {
+            return txtTitulo;
+        }
+    }
+
+
+    public void setSections(Meses[] sections) {
+        mSections.clear();
+
+        Arrays.sort(sections, new Comparator<Meses>() {
+            @Override
+            public int compare(Meses o, Meses o1) {
+                return Integer.compare(o.firstPosition, o1.firstPosition);
+            }
+        });
+
+        int offset = 0; // offset positions for the headers we're adding
+        for (Meses section : sections) {
+            section.sectionedPosition = section.firstPosition + offset;
+            mSections.append(section.sectionedPosition, section);
+            ++offset;
+        }
+
+        notifyDataSetChanged();
+    }
+
+    public int sectionedPositionToPosition(int sectionedPosition) {
+        if (isSectionHeaderPosition(sectionedPosition)) {
+            return RecyclerView.NO_POSITION;
+        }
+        int offset = 0;
+        for (int i = 0; i < mSections.size(); i++) {
+            if (mSections.valueAt(i).sectionedPosition > sectionedPosition) {
+                break;
+            }
+            --offset;
+        }
+        return sectionedPosition + offset;
+    }
+
+    public boolean isSectionHeaderPosition(int position) {
+        return mSections.get(position) != null;
+    }
+
+
+    @Override
+    public long getItemId(int position) {
+        return isSectionHeaderPosition(position)
+                ? Integer.MAX_VALUE - mSections.indexOfKey(position)
+                : mBaseAdapter.getItemId(sectionedPositionToPosition(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return (mValid ? clases.size() + mSections.size() : 0);
+    }
+
+
+
+}
