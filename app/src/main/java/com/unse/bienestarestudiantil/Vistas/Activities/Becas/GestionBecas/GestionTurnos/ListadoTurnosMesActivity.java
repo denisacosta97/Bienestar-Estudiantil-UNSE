fd6ce688beadd22904f5729288d3409c77b9e30a
ledesmacs <ledesmacristiansantiago@gmail.com>
2020-10-26 22:53:15 -0300

    La clase que faltaba gg

---
+++ app/src/main/java/com/unse/bienestarestudiantil/Vistas/Activities/Becas/GestionBecas/GestionTurnos/ListadoTurnosMesActivity.java
@@ -0,0 +1,155 @@
+package com.unse.bienestarestudiantil.Vistas.Activities.Becas.GestionBecas.GestionTurnos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.unse.bienestarestudiantil.Herramientas.Almacenamiento.PreferenceManager;
+import com.unse.bienestarestudiantil.Herramientas.Utils;
+import com.unse.bienestarestudiantil.Herramientas.VolleySingleton;
+import com.unse.bienestarestudiantil.Modelos.Turno;
+import com.unse.bienestarestudiantil.R;
+import com.unse.bienestarestudiantil.Vistas.Dialogos.DialogoProcesamiento;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ListadoTurnosMesActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Turno mTurnos, mTurnoComplete;
+    DialogoProcesamiento dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_turnos_mes);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        if (getIntent().getParcelableExtra(Utils.DATA_RESERVA) != null) {
+            mTurnos = getIntent().getParcelableExtra(Utils.DATA_RESERVA);
+        }
+
+        setToolbar();
+
+        loadViews();
+
+        loadListener();
+
+        loadData();
+
+        loadInfo();
+    }
+
+    private void setToolbar() {
+        ((TextView) findViewById(R.id.txtTitulo)).setText(Utils.getAppName(getApplicationContext(), getComponentName()));
+
+    }
+
+
+    private void loadListener() {
+
+    }
+
+    private void loadData() {
+
+    }
+
+    private void loadViews() {
+
+    }
+
+    private void loadInfo() {
+        PreferenceManager manager = new PreferenceManager(getApplicationContext());
+        String key = manager.getValueString(Utils.TOKEN);
+        int id = manager.getValueInt(Utils.MY_ID);
+        String URL = String.format("%s?idU=%s&key=%s", Utils.URL_TURNOS_LISTA, id, key);
+        StringRequest request = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                procesarRespuesta(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                Utils.showToast(getApplicationContext(), getString(R.string.servidorOff));
+                dialog.dismiss();
+
+            }
+        });
+        //Abro dialogo para congelar pantalla
+        dialog = new DialogoProcesamiento();
+        dialog.setCancelable(false);
+        dialog.show(getSupportFragmentManager(), "dialog_process");
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
+    }
+
+    private void procesarRespuesta(String response) {
+        try {
+            dialog.dismiss();
+            JSONObject jsonObject = new JSONObject(response);
+            int estado = jsonObject.getInt("estado");
+            switch (estado) {
+                case -1:
+                    Utils.showToast(getApplicationContext(), getString(R.string.errorInternoAdmin));
+                    break;
+                case 1:
+                    //Exito
+                    loadInfo(jsonObject);
+                    break;
+                case 2:
+                    Utils.showToast(getApplicationContext(), getString(R.string.noData));
+                    break;
+                case 3:
+                    Utils.showToast(getApplicationContext(), getString(R.string.tokenInvalido));
+                    break;
+                case 100:
+                    //No autorizado
+                    Utils.showToast(getApplicationContext(), getString(R.string.tokenInexistente));
+                    break;
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Utils.showToast(getApplicationContext(), getString(R.string.errorInternoAdmin));
+        }
+    }
+
+    private void loadInfo(JSONObject jsonObject) {
+        try {
+            if (jsonObject.has("mensaje")) {
+
+                JSONArray jsonArray = jsonObject.getJSONArray("mensaje");
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+
+                    JSONObject o = jsonArray.getJSONObject(i);
+
+                    Turno turno = Turno.mapper(o, 2);
+
+                }
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imgFlecha:
+                onBackPressed();
+                break;
+        }
+    }
+}
